c2e113849ea9a633bdb719b66505f4bad142b360
Madi <madi.nurkenov@gmail.com>
2021-04-09 20:01:06 +0600

    newest not working properly version

--- ship.cpp
+++ ship.cpp
@@ -1,6 +1,8 @@
 #include "ship.h"
 #define endl << endl
 
+
+
 void ship::generateFields(){ // добавить цифры у сторон поля
     for(int i = 0; i < 12; i++){
         for(int j = 0; j < 13; j++){
@@ -19,16 +21,27 @@ void ship::generateFields(){ // добавить цифры у сторон по
 }
 
 void ship::printUI(){
+    cout << "   12345678910\t \t   12345678910" endl;
     for(int i = 0; i < 12; i++){
-        cout << playerUI[i] << "\t \t" << botUI[i] endl;
+        cout << i;
+        if(i < 10){
+            cout << " ";
+        }
+        cout << playerUI[i] << "\t \t" << i;
+        if(i < 10){
+            cout << " ";
+        }
+        cout << botUI[i] endl;
+        // cout << i << playerUI[i] << "\t \t" << i << botUI[i] endl;
     }
 }
 
 void ship::setShipDirection() {
     if(start[0] == end[0]) {
-        shipDirectin = "horizontal";
+        shipDirection = "horizontal";
+        return;
     }
-    shipDirectin = "vertical";
+    shipDirection = "vertical";
 }
 
 bool ship::askCoordinates(char choice) {
@@ -58,16 +71,24 @@ bool ship::askCoordinates(char choice) {
                 }
             }
         }
-    } else {
-        if(shipDirectin == "horizontal") {
+    } else if(choice == 'D'){ 
+        if(shipDirection == "horizontal") {
             for(int i = start[1]; i <= end[1]; i++) {
+                if(playerUI[start[0] + 1][i] == '*' || playerUI[start[0] - 1][i] == '*'){
+                    cout << "Can't delete a part of the ship" endl;
+                    return false;
+                }
                 if(playerUI[start[0]][i] != '*') {
                     cout << "Incorrect coordinates, there is no ships!" endl;
                     return false;
                 }
             }
-        } else if(shipDirectin == "vertical") {
+        } else if(shipDirection == "vertical") {
             for(int i = start[0]; i <= end[0]; i++) {
+                if(playerUI[i][start[1] + 1] == '*' || playerUI[i][start[1] - 1] == '*'){
+                    cout << "Can't delete a part of the ship asd" endl;
+                    return false;
+                }
                 if(playerUI[i][start[1]] != '*') {
                     cout << "Incorrect coordinates, there is no ships!" endl;
                     return false;
@@ -79,8 +100,259 @@ bool ship::askCoordinates(char choice) {
     return true;
 }
 
+bool ship::botShipCoordsChecker(){
+    for(int i = -1; i <= 1; i++){
+        for(int j = -1; j <= 1; j++){
+            if(botUI[start[0] + i][start[1] + j] == '*' || botUI[end[0] + i][end[1] + j] == '*' || start[0] < 1 || start[0] > 10 || start[1] < 1 || start[1] > 10 || end[0] < 1 || end[0] > 10 || end[1] < 1 || end[1] > 10){
+                cout << "worked" endl;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+void ship::generateBotCoords(){
+    int direction, shipType;
+
+    
+    
+    for(int i = 0; i < 4; i++){
+        cout << i endl;
+        start[0] = rand()%10 + 1;
+        start[1] = rand()%10 + 1;
+        end[0] = start[0];
+        end[1] = start[1];
+        if(botShipCoordsChecker()){
+            i--;
+            cout << "Coords: " << start[0] << " " << start[1] << " " << end[0] << " " << end[1] endl << "Ship type: 1" endl; 
+        } else{
+            createBotShip();
+        }
+        printUI();
+    }
+    
+    for(int i = 0; i < 3; i++){
+        direction = rand()%5 + 1;
+        switch(direction){
+            case 1: // up
+                shipDirection = "vertical";
+                start[0] = rand()%10 + 1;
+                start[1] = rand()%10 + 1;
+                end[0] = start[0] - 1;
+                end[1] = start[1];
+                if(botShipCoordsChecker()){
+                    i--;
+                    cout << "Coords: " << start[0] << " " << start[1] << " " << end[0] << " " << end[1] endl << "Ship type: 2" endl; 
+                } else{
+                    createBotShip();
+                }
+                break;
+            case 2: // down
+                shipDirection = "vertical";
+                start[0] = rand()%10 + 1;
+                start[1] = rand()%10 + 1;
+                end[0] = start[0] + 1;
+                end[1] = start[1];
+                if(botShipCoordsChecker()){
+                    i--;
+                    cout << "Coords: " << start[0] << " " << start[1] << " " << end[0] << " " << end[1] endl << "Ship type: 2" endl; 
+                } else{
+                    createBotShip();
+                }
+                break;
+            case 3: // left
+                shipDirection = "horizontal";
+                start[0] = rand()%10 + 1;
+                start[1] = rand()%10 + 1;
+                end[0] = start[0];
+                end[1] = start[1] - 1;
+                if(botShipCoordsChecker()){
+                    i--;
+                    cout << "Coords: " << start[0] << " " << start[1] << " " << end[0] << " " << end[1] endl << "Ship type: 2" endl; 
+                } else{
+                    createBotShip();
+                }
+                break;
+            case 4: // right
+                shipDirection = "horizontal";
+                start[0] = rand()%10 + 1;
+                start[1] = rand()%10 + 1;
+                end[0] = start[0];
+                end[1] = start[1] + 1;
+                if(botShipCoordsChecker()){
+                    i--;
+                    cout << "Coords: " << start[0] << " " << start[1] << " " << end[0] << " " << end[1] endl << "Ship type: 2" endl; 
+                } else{
+                    createBotShip();
+                }
+                break;
+        }
+        printUI();
+    }
+    
+    for(int i = 0; i < 2; i++){
+        direction = rand()%5 + 1;
+        switch(direction){
+            case 1: // up
+                shipDirection = "vertical";
+                start[0] = rand()%10 + 1;
+                start[1] = rand()%10 + 1;
+                end[0] = start[0] - 2;
+                end[1] = start[1];
+                if(botShipCoordsChecker()){
+                    i--;
+                    cout << "Coords: " << start[0] << " " << start[1] << " " << end[0] << " " << end[1] endl << "Ship type: 3" endl; 
+                } else{
+                    createBotShip();
+                }
+                break;
+            case 2: // down
+                shipDirection = "vertical";
+                start[0] = rand()%10 + 1;
+                start[1] = rand()%10 + 1;
+                end[0] = start[0] + 2;
+                end[1] = start[1];
+                if(botShipCoordsChecker()){
+                    i--;
+                    cout << "Coords: " << start[0] << " " << start[1] << " " << end[0] << " " << end[1] endl << "Ship type: 3" endl; 
+                } else{
+                    createBotShip();
+                }
+                break;
+            case 3: // left
+                shipDirection = "horizontal";
+                start[0] = rand()%10 + 1;
+                start[1] = rand()%10 + 1;
+                end[0] = start[0];
+                end[1] = start[1] - 2;
+                if(botShipCoordsChecker()){
+                    i--;
+                    cout << "Coords: " << start[0] << " " << start[1] << " " << end[0] << " " << end[1] endl << "Ship type: 3" endl; 
+                } else{
+                    createBotShip();
+                }
+                break;
+            case 4: // right
+                shipDirection = "horizontal";
+                start[0] = rand()%10 + 1;
+                start[1] = rand()%10 + 1;
+                end[0] = start[0];
+                end[1] = start[1] + 2;
+                if(botShipCoordsChecker()){
+                    i--;
+                    cout << "Coords: " << start[0] << " " << start[1] << " " << end[0] << " " << end[1] endl << "Ship type: 3" endl; 
+                } else{
+                    createBotShip();
+                }
+                break;
+        }
+        printUI();
+    }
+    
+    direction = rand()%5 + 1;
+    for(int i = 0; i < 1; i++){
+    switch(direction){
+        case 1: // up
+            shipDirection = "vertical";
+            start[0] = rand()%10 + 1;
+            start[1] = rand()%10 + 1;
+            end[0] = start[0] - 3;
+            end[1] = start[1];
+            if(botShipCoordsChecker()){
+                i--;
+                cout << "Coords: " << start[0] << " " << start[1] << " " << end[0] << " " << end[1] endl << "Ship type: 4" endl; 
+            } else{
+                createBotShip();
+            }
+            break;
+        case 2: // down
+            shipDirection = "vertical";
+            start[0] = rand()%10 + 1;
+            start[1] = rand()%10 + 1;
+            end[0] = start[0] + 3;
+            end[1] = start[1];
+            if(botShipCoordsChecker()){
+                i--;
+                cout << "Coords: " << start[0] << " " << start[1] << " " << end[0] << " " << end[1] endl << "Ship type: 4" endl; 
+            } else{
+                createBotShip();
+            }
+            break;
+        case 3: // left
+            shipDirection = "horizontal";
+            start[0] = rand()%10 + 1;
+            start[1] = rand()%10 + 1;
+            end[0] = start[0];
+            end[1] = start[1] - 3;
+            if(botShipCoordsChecker()){
+                i--;
+                cout << "Coords: " << start[0] << " " << start[1] << " " << end[0] << " " << end[1] endl << "Ship type: 4" endl; 
+            } else{
+                createBotShip();
+            }
+            break;
+        case 4: // right
+            shipDirection = "horizontal";
+            start[0] = rand()%10 + 1;
+            start[1] = rand()%10 + 1;
+            end[0] = start[0];
+            end[1] = start[1] + 3;
+            if(botShipCoordsChecker()){
+                i--;
+                cout << "Coords: " << start[0] << " " << start[1] << " " << end[0] << " " << end[1] endl << "Ship type: 4" endl; 
+            } else{
+                createBotShip();
+            }
+            break;
+    }
+    printUI();
+    }
+    
+    
+}
+void ship::createBotShip(){
+    if(shipDirection == "horizontal"){ 
+        shipLength = end[1] - start[1];
+        if(botShips[shipLength] > 0){
+            if(shipLength >= 0) {
+                for(int i = start[1]; i <= end[1]; i++){
+                    botUI[start[0]][i] = '*';
+                } 
+                botShips[shipLength]--;
+            } else if(shipLength < 0) {
+                for(int i = start[1]; i >= end[1]; i--){  
+                    botUI[start[0]][i] = '*';
+                }
+                botShips[abs(shipLength)]--;
+            }
+        } else{
+            cout << "Maximum amount of this type of ships reached." endl;
+        }
+        
+    }
+    else if(shipDirection == "vertical"){
+        shipLength = end[0] - start[0];
+        if(botShips[shipLength] > 0){
+            if(shipLength >= 0) {
+                for(int i = start[0]; i <= end[0]; i++){
+                    botUI[i][start[1]] = '*';
+                }
+                botShips[shipLength]--;
+            } else if(shipLength < 0) {
+                for(int i = start[0]; i >= end[0]; i--){
+                    botUI[i][start[1]] = '*';
+                }
+                botShips[shipLength]--;
+            }
+        } else{
+            cout << "Maximum amount of this type of ships reached." endl;
+        }
+    }
+}
+
 void ship::createShip(){
-    if(shipDirectin == "horizontal"){ // добавить возможность изменить положение кораблей до битвы, почему то ставятся рядом, но не ставятся вдали друг от друга. 
+    if(shipDirection == "horizontal"){ 
         shipLength = end[1] - start[1];
         if(ships[shipLength] > 0){
             if(shipLength >= 0) {
@@ -99,7 +371,7 @@ void ship::createShip(){
         }
         
     }
-    else if(shipDirectin == "vertical"){
+    else if(shipDirection == "vertical"){
         shipLength = end[0] - start[0];
         if(ships[shipLength] > 0){
             if(shipLength >= 0) {
@@ -120,12 +392,61 @@ void ship::createShip(){
 }
 
 void ship::deleteShip(){
+    if(shipDirection == "horizontal"){ 
+        shipLength = end[1] - start[1];
+        if(ships[shipLength] < 4 - shipLength){
+            if(shipLength >= 0) {
+                for(int i = start[1]; i <= end[1]; i++){
+                    playerUI[start[0]][i] = ' ';
+                } 
+                ships[shipLength]++;
+            } else if(shipLength < 0) {
+                for(int i = start[1]; i >= end[1]; i--){  
+                    playerUI[start[0]][i] = ' ';
+                }
+                ships[abs(shipLength)]++;
+            }
+        } else{
+            cout << "No ships of that type on the board." endl;
+        }
+        
+    }
+    else if(shipDirection == "vertical"){
+        shipLength = end[0] - start[0];
+        if(ships[shipLength] < 4 - shipLength){
+            if(shipLength >= 0) {
+                for(int i = start[0]; i <= end[0]; i++){
+                    playerUI[i][start[1]] = ' ';
+                }
+                ships[shipLength]++;
+            } else if(shipLength < 0) {
+                for(int i = start[0]; i >= end[0]; i--){
+                    playerUI[i][start[1]] = ' ';
+                }
+                ships[shipLength]++;
+            }
+        } else{
+            cout << "No ships of that type on the board." endl;
+        }
+    }
+}
 
+bool ship::fieldStatusCheck(){
+    char status;
+    if(!(ships[0] != 0 || ships[1] != 0 || ships[2] != 0 || ships[3] != 0)){
+        printUI();
+        cout << "Is this your final field? (Y/N)" endl;
+        cin >> status;
+        if(status == 'Y'){
+            return false;
+        } 
+    }
+    return true;
 }
 
 void ship::setUserShips(){      // 4 одинарных, 3 двойних, 2 тройных, 1 четверной
     char choice;                // 0 - одинарн., 1 - двойн., 2 - тройн., 3 - четверн.
-    while(ships[0] != 0 || ships[1] != 0 || ships[2] != 0 || ships[3] != 0){
+    while(fieldStatusCheck()){ // добавить подтверждение к завершению расстановки
         printUI();
         cout << "What do you want to do? Place a ship (P), Delete a ship (D)." endl;
         cin >> choice;
@@ -156,9 +477,15 @@ void ship::makeMove(){
 }
 
 ship::ship(){
+    srand(time(0));
     generateFields();
-    setUserShips();
+    // setUserShips();
     printUI();    
+    generateBotCoords();
+    for(int i = 0; i < 4; i++){
+        cout << botShips[i] << " ";
+    }
+    cout endl;
 }
 
 
--- ship.h
+++ ship.h
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <math.h>
+#include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 class ship{
@@ -7,18 +9,23 @@ class ship{
     char botUI[12][13]; 
     int start[2], end[2];
     int ships[4] = {4, 3, 2, 1};
-    string shipDirectin;
+    int botShips[4] = {4, 3, 2, 1};
+    string shipDirection = "horizontal";
     int shipLength;
 
     void setShipDirection();
     bool askCoordinates(char choice);
     void generateFields();
+    bool botShipCoordsChecker();
+    void generateBotCoords();
+    void createBotShip();
     void createShip();
     void deleteShip();
     void printUI();
     void setUserShips();
     void askMove();
     void makeMove();
+    bool fieldStatusCheck();
     public:
     ship();
 
